811441836b88476a5f6a6eee85d049c3773b352c
chenyuan <yuany.chen@qunar.com>
2018-03-09 20:07:08 +0800

    拖拽元素

--- src/index.js
+++ src/index.js
@@ -60,6 +60,31 @@ class SpiderFlow {
         }
         await sleep(sleepTime);
 
+        // 为page添加函数
+        page.touchscreen.swipePoints = async function (x, y, points) {
+            await this._client.send('Input.dispatchTouchEvent', {
+                type: 'touchStart',
+                touchPoints: [{ x: Math.round(x), y: Math.round(y) }],
+                modifiers: this._keyboard._modifiers
+            });
+
+            for (let i = 0; i < points.length; i++) {
+                await this._client.send('Input.dispatchTouchEvent', {
+                    type: 'touchMove',
+                    touchPoints: [{ x: points[i].toX, y: points[i].toY }],
+                    modifiers: this._keyboard._modifiers
+                });
+                await sleep(points[i].timespan);
+            }
+            await sleep(50);
+
+            await this._client.send('Input.dispatchTouchEvent', {
+                type: 'touchEnd',
+                touchPoints: [],
+                modifiers: this._keyboard._modifiers
+            });
+        }.bind(page.touchscreen);
+
         // 设置cookie
         if (cookies.length > 0) {
             await page.setCookie(...cookies);
@@ -111,6 +136,85 @@ class SpiderFlow {
             // 输入值
             await page.type(selector, value, { delay: 100 });
         }
+
+        // 执行回调函数
+        await callback(this);
+
+        // 关闭页面
+        if (closePage) {
+            await page.close();
+            this.pages[pageSymbol] = null;
+        }
+    }
+
+    // 点击
+    async click(config, callback) {
+        let {
+            pageSymbol = Symbol('page'), // 要使用的页面
+            sleepTime = 200, // 毫秒
+            cookies = [],     // cookie
+            clicks = [],      // 要输入的内容
+            closePage = false  // 操作完成后是否关闭页面
+        } = config;
+
+        // 获取页面
+        let page = this.pages[pageSymbol];
+        if (!page) {
+            throw new Error('页面不存在');
+        }
+        await sleep(sleepTime);
+
+        // 设置input的值
+        for (let i = 0; i< clicks.length; i++) {
+            const {
+                selector
+            } = clicks[i];
+            // 查看元素是否存在
+            const element = this.existElement(page, selector);
+            if(!element) {
+                throw new Error('元素不存在');
+            }
+            // 点击元素
+            await page.click(selector);
+        }
+
+        // 执行回调函数
+        await callback(this);
+
+        // 关闭页面
+        if (closePage) {
+            await page.close();
+            this.pages[pageSymbol] = null;
+        }
+    }
+
+    // 拖动
+    async slide(config, callback) {
+        let {
+            pageSymbol = Symbol('page'), // 要使用的页面
+            sleepTime = 200, // 毫秒
+            selector = '', // 要拖拽的元素
+            points = [],     // 拖拽轨迹
+            closePage = false  // 操作完成后是否关闭页面
+        } = config;
+
+        // 获取页面
+        let page = this.pages[pageSymbol];
+        if (!page) {
+            throw new Error('页面不存在');
+        }
+        await sleep(sleepTime);
+        // 查看元素是否存在
+        const element = this.existElement(page, selector);
+        if(!element) {
+            throw new Error('元素不存在');
+        }
+
+        // 拖拽元素
+        //const e = await page.$(selector);
+        //const box = await e.boundingBox();
+        await page.touchscreen.swipePoints(points[0].toX, points[0].toY, points);
+
 
         // 执行回调函数
         await callback(this);
--- test/index.js
+++ test/index.js
@@ -60,8 +60,111 @@ async function test() {
 
 
     });
-
     console.log("result2:", result2);
+
+    // 点击按钮
+    let result3 = await t.step({
+        operation: 'click',
+        pageSymbol: result1.pageSymbol,
+        clicks: [{
+            selector: '#su'
+        }],
+        closePage: false
+    }, async function (ctx) {
+
+    });
+    console.log("result3:", result3);
+
+    // 滑动
+    // 点击按钮
+    let result4 = await t.step({
+        operation: 'slide',
+        pageSymbol: result1.pageSymbol,
+        selector: '',
+        points: [
+            {
+                timespan: 0,
+                toX: 116.93399810791016,
+                toY: 345.2229919433594
+            },
+            {
+                timespan: 91,
+                toX: 139.94500732421875,
+                toY: 345.2229919433594
+            },
+            {
+                timespan: 99,
+                toX: 151.02000427246094,
+                toY: 345.2229919433594
+            },
+            {
+                timespan: 101,
+                toX: 154.6840057373047,
+                toY: 345.2229919433594
+            },
+            {
+                timespan: 99,
+                toX: 165.66400146484375,
+                toY: 345.2229919433594
+            },
+            {
+                timespan: 101,
+                toX: 197.07400512695312,
+                toY: 346.5270080566406
+            },
+            {
+                timespan: 99,
+                toX: 211.6909942626953,
+                toY: 347.24200439453125
+            },
+            {
+                timespan: 101,
+                toX: 223.57000732421875,
+                toY: 348.218994140625
+            },
+            {
+                timespan: 100,
+                toX: 230.156005859375,
+                toY: 348.7969970703125
+            },
+            {
+                timespan: 99,
+                toX: 235.6909942626953,
+                toY: 349.3479919433594
+            },
+            {
+                timespan: 100,
+                toX: 239.718994140625,
+                toY: 349.8710021972656
+            },
+            {
+                timespan: 100,
+                toX: 245,
+                toY: 350
+            },
+            {
+                timespan: 101,
+                toX: 248.97300720214844,
+                toY: 350.67999267578125
+            },
+            {
+                timespan: 100,
+                toX: 251.53500366210938,
+                toY: 350.9410095214844
+            },
+            {
+                timespan: 147,
+                toX: 253.7729949951172,
+                toY: 351.4100036621094
+            }
+        ],
+        closePage: false
+    }, async function (ctx) {
+
+
+    });
+
+    console.log("result4:", result4);
 }
 
 test();
